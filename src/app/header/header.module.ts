c554562e1a88698442827fb10f2989e69e4b3010
Marcos Winiarski <marcos.winiarski@mv.com.br>
2019-05-21 17:08:25 -0300

    Modified architecture to supporte internacionalization.

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialUiModule } from '../core/material/material-ui.module';
-import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -10,9 +9,7 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     MaterialUiModule,
     RouterModule
-  ],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  ]
 })
 export class HeaderModule { }
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialUiModule } from '../core/material/material-ui.module';
-import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialUiModule
-  ],
-  declarations: [LoginComponent],
-  exports: [LoginComponent]
+  ]
 })
 export class LoginModule { }
