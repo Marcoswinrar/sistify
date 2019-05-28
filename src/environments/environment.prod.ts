620f86287db3c6bf9d6d34d9a052c06236038181
Marcos Winiarski <marcos.winiarski@mv.com.br>
2019-05-28 10:10:28 -0300

    Adding uri of redirect for production in enviroment,

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   client_id : '498d8f9abfa64bf2892a9808ef3938ce',
-  redirect_uri: 'http://localhost:4200/user',
+  redirect_uri: 'https://sistify-app.netlify.com',
   authorization_url : 'https://accounts.spotify.com/authorize',
   api: 'https://api.spotify.com/v1'
 };
